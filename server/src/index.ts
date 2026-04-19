82597843c4b2641453ec7cdf31f4c51f7beb672f
Andrea Sciortino <andreasciortino2004@gmail.com>
2026-04-20 00:33:10 +0200

    add better sqlite3

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import sqlite3 from "sqlite3";
+import Database from "better-sqlite3";
 import cors from "cors";
 
 import { execute, fetchAll, fetchFirst } from "./sql";
@@ -14,77 +14,92 @@ app.use(
   }),
 );
 
-const db = new sqlite3.Database("test.db");
+const db = new Database("test.db");
+db.pragma("journal_mode = WAL");
 
 app.get("/", async (req, res) => {
   res.send("Hello World!");
-  const rows = await fetchAll(db, "SELECT * FROM User");
-  console.log(rows);
+
+  const stmt = db.prepare("SELECT * FROM User");
+  const user = stmt.get();
+
+  console.log(user);
+
+  // const rows = await fetchAll(db, "SELECT * FROM User");
+  // console.log(rows);
 });
 
 app.get("/user/:userId", async (req, res) => {
   const userId = req.params.userId;
-  const row = await fetchFirst(
-    db,
+
+  const stmt = db.prepare(
     "SELECT user_id, username, email FROM User WHERE user_id = ?",
-    [userId],
   );
-  console.log(row);
-  res.send({ user: row });
+  const user = stmt.get(userId);
+
+  console.log(user);
+  res.send({ user: user });
+  // const row = await fetchFirst(
+  //   db,
+  //   "SELECT user_id, username, email FROM User WHERE user_id = ?",
+  //   [userId],
+  // );
+  // console.log(row);
+  // res.send({ user: row });
 });
 
-app.get("/list/:userId/entrie/:animeId", async (req, res) => {
-  const { userId, animeId } = req.params;
+// app.get("/list/:userId/entrie/:animeId", async (req, res) => {
+//   const { userId, animeId } = req.params;
 
-  const row = await fetchFirst(
-    db,
-    "SELECT * FROM 'Private Anime' WHERE user_id = ? AND anime_id = ?",
-    [userId, animeId],
-  );
-  console.log(row);
-  if (row === undefined) {
-    res.send({ entrie: null });
-  } else {
-    res.send({ entrie: row });
-  }
-});
+//   const row = await fetchFirst(
+//     db,
+//     "SELECT * FROM 'Private Anime' WHERE user_id = ? AND anime_id = ?",
+//     [userId, animeId],
+//   );
+//   console.log(row);
+//   if (row === undefined) {
+//     res.send({ entrie: null });
+//   } else {
+//     res.send({ entrie: row });
+//   }
+// });
 
-app.post("/list/:userId/entrie", async (req, res) => {
-  const { userId } = req.params;
-  const { animeId, status } = req.body;
+// app.post("/list/:userId/entrie", async (req, res) => {
+//   const { userId } = req.params;
+//   const { animeId, status } = req.body;
 
-  const query = await execute(
-    db,
-    "INSERT INTO 'Private Anime'(user_id,status,anime_id,added_on) VALUES(?,?,?, datetime('now'))",
-    [userId, status, animeId],
-  );
+//   const query = await execute(
+//     db,
+//     "INSERT INTO 'Private Anime'(user_id,status,anime_id,added_on) VALUES(?,?,?, datetime('now'))",
+//     [userId, status, animeId],
+//   );
 
-  res.send({ message: "Added" });
-});
+//   res.send({ message: "Added" });
+// });
 
-app.patch("/list/:userId/entrie", async (req, res) => {
-  const { userId } = req.params;
-  const { animeId, status } = req.body;
+// app.patch("/list/:userId/entrie", async (req, res) => {
+//   const { userId } = req.params;
+//   const { animeId, status } = req.body;
 
-  const query = await execute(
-    db,
-    "UPDATE 'Private Anime' SET status = ? WHERE user_id = ? AND anime_id = ?",
-    [status, userId, animeId],
-  );
+//   const query = await execute(
+//     db,
+//     "UPDATE 'Private Anime' SET status = ? WHERE user_id = ? AND anime_id = ?",
+//     [status, userId, animeId],
+//   );
+//   console.log("path", query);
+//   res.send({ message: "Updated" });
+// });
 
-  res.send({ message: "Updated" });
-});
+// app.delete("/list/:userId/entrie/:animeId", async (req, res) => {
+//   const { userId, animeId } = req.params;
+//   const query = await execute(
+//     db,
+//     "DELETE FROM 'Private Anime' WHERE user_id = ? AND anime_id = ?",
+//     [userId, animeId],
+//   );
 
-app.delete("/list/:userId/entrie/:animeId", async (req, res) => {
-  const { userId, animeId } = req.params;
-  const query = await execute(
-    db,
-    "DELETE FROM 'Private Anime' WHERE user_id = ? AND anime_id = ?",
-    [userId, animeId],
-  );
-
-  res.send({ message: "Deleted" });
-});
+//   res.send({ message: "Deleted" });
+// });
 
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
